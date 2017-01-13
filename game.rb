d0a2d687a1649909c806b4f6c87f691443c898a7
Guillaume <nomine.g@gmail.com>
2017-01-13 14:51:18 +0100

    deplacement animaux

--- game.rb
+++ game.rb
@@ -1,12 +1,37 @@
 #Donovan
+def throw_dice
+    result = rand 1..3
+    if result == 1 
+        return "igloo"
+    elsif result == 2
+        return "bridge"
+    else
+        return "pillar"
+    end
+end
+
+
 #Ernst
 #creation du pont 
 coldbridge =["pilier", "pilier", "pilier","pilier", "pilier", "pilier"]
+
+
 #Amara
+# Définir les animaux dans leur lieux 
+animaux ={
+          ours: "banquise",
+          lapin: "banquise",
+          pingouin: "banquise",
+          renard: "banquise"
+} 
+
 
 #Guillaume
 lieux = ["banquise", "pont", "igloo"]
 
-#Guilluame
+#fonction deplacement animaux
+def move_animal animals, destination, animaux
 
+     animaux[animals] = destination
+end 
 
