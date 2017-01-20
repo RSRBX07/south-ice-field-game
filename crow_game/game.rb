a2d4779cfe141f93ce7b0c63994a697ca3583602
patpbe <pat.pbe@instanweb.fr>
2017-01-20 15:03:35 +0100

    add choose_fruit, remove_fruit (work in progress)

--- crow_game/game.rb
+++ crow_game/game.rb
@@ -6,7 +6,26 @@ class Game
 
 
 #Pat
+def fruit_number_to number
+  number
+end
 
+def choose_fruit
+  #retourne le numéro du fruit choisi
+  begin  
+    puts "Choisissez un fruit en entrant un chiffre:"
+    puts "1: Red Apple"
+    puts "2: Green Apple"
+    puts "3: Pears"
+    puts "4: Plum"
+    input = gets.to_i
+  end until (1..4).member?(input)
+  fruit_number_to input
+end
+
+def remove_fruit fruit
+  #fruit and tree remove
+end
 
 
 
@@ -22,3 +41,6 @@ class Game
 
 
 end
+
+g = Game.new
+puts g.choose_fruit
