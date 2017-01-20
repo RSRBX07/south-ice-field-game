7c60943e06b9d93597101ac7a678cc72994357b5
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-20 12:14:51 +0100

    add methods

--- crow_game/crow.rb
+++ crow_game/crow.rb
@@ -6,5 +6,15 @@ class crow
     @advancement += 1
   end
 
+  def initialize
+    advancement = 0    
+  end
+  def win
+    puts " j'ai volé tes fruits"
+  end
+  def loose
+     puts"vous avez gagné"
+  end
+
 end
 
