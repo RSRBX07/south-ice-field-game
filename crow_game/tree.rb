5b82a0cdc9328e6bac3ba042eca26f89dd34d8f5
patpbe <pat.pbe@instanweb.fr>
2017-01-20 16:13:51 +0100

    update tree (wip)

--- crow_game/tree.rb
+++ crow_game/tree.rb
@@ -1,10 +1,22 @@
+require 'fruit.rb'
 class Tree
-  attr_accessor :fruits_count
+  attr_accessor :fruits
   attr_accessor :tree_kind
   
   def initialize a_tree_kind
-    @fruits_count = 4
+    color = fruit_color a_tree_kind
+    @fruits = []
+    @fruits << 4.times {Fruit.new color}
     @tree_kind = a_tree_kind
+  end
+
+  def fruit_color a_tree_kind
+    case a_tree_kind
+    when :Red_Apple_Tree return Fruit::COLORS[:red]
+    when :Green_Apple_Tree return Fruit::COLORS[:green]
+    when :Pears_Tree return Fruit::COLORS[:yellow]
+    when :Plum_Tree return Fruit::COLORS[:purple]
+    end
   end
 
   def no_more_fruits?
@@ -12,28 +24,6 @@ class Tree
   end
 
   def remove_fruit
-    @fruits_count -= 1 unless no_more_fruits?
+    @fruits.pop.gather unless no_more_fruits?
   end
 end
-
-# tree1 = Tree.new :Red_Apple_Tree
-# tree2 = Tree.new :Green_Apple_Tree
-# tree3 = Tree.new :Pears_Tree
-# tree4 = Tree.new :Plum_Tree
-# puts tree1.inspect
-# puts tree2.inspect
-# puts tree3.inspect
-# puts tree4.inspect
-
-# tree1 = Tree.new :Red_Apple_Tree
-# puts tree1.inspect
-# tree1.remove_fruit
-# puts tree1.inspect
-# tree1.remove_fruit
-# puts tree1.inspect
-# tree1.remove_fruit
-# puts tree1.inspect
-# tree1.remove_fruit
-# puts tree1.inspect
-# tree1.remove_fruit
-# puts tree1.inspect
