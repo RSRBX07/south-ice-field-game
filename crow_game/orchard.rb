f6b768de7ee775dd7adf21af57cb8270802d5afa
patpbe <pat.pbe@instanweb.fr>
2017-01-20 14:27:38 +0100

    add_orchard

---
+++ crow_game/orchard.rb
@@ -0,0 +1,11 @@
+class Orchard
+
+  def initialize red_apple_tree, green_apple_tree, pears_tree, plum_tree
+    @trees = {:Red_Apple_Tree => red_apple_tree,
+              :Green_Apple_Tree => green_apple_tree,
+              :Pears_Tree => pears_tree,
+              :Plum_Tree => plum_tree
+    }
+  end
+
+end
